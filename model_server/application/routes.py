59137351b6567f0f12e70e65ac4d8381a83f0b79
ruiyuanqian <u3556256@connect.hku.hk>
2020-08-06 14:04:59 +0800

    Add a simple rnn pkg to classify text as emotions

--- model_server/application/routes.py
+++ model_server/application/routes.py
@@ -2,6 +2,8 @@ from application import app, pathName
 from flask import request, jsonify
 import json
 
+from twitteremotionrecognition import predictOneSentence
+
 @app.route( '/{}/echo/'.format( pathName ) , methods=['GET'] )
 def doGet_echo():
     return 'hello world\n'
@@ -9,3 +11,17 @@ def doGet_echo():
 @app.route( '/{}/echo/'.format( pathName ) , methods=['POST'] )
 def doPost_echo():
     return jsonify( json.loads( request.data ) )
+
+@app.route( '/{}/rnn/'.format( pathName ) , methods=['GET'] )
+def doGet_rnn():
+    sentence = request.args.get('s','')
+    if( len(sentence) > 0 ):
+        return str( predictOneSentence( sentence ) )
+    return 'ok'
+
+@app.route( '/{}/rnn/'.format( pathName ) , methods=['POST'] )
+def doPost_rnn():
+    sentence = json.loads( request.data ).get('sentence','')
+    if( len(sentence) > 0 ):
+        return str( predictOneSentence( sentence ) )
+    return 'ok'
