c2008e6f1bf774fffdd963d683512f8eb62b24df
ruiyuanqian <u3556256@connect.hku.hk>
2020-08-28 13:00:31 +0000

    Give model server's input and output a schema

--- model_server/application/routes.py
+++ model_server/application/routes.py
@@ -33,12 +33,37 @@ def doGet_rnn():
     return render_template( 'index.html' , pathOfEmotionClassification = fullPathToRnn )
 
 
+import traceback
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
 @app.route( '/{}/rnn/'.format( pathName ) , methods=['POST'] )
 def doPost_rnn():
-    RNNStrToEmotion = loadTheRNNModel()
-    sentence = ( request.json ).get( 'sentence' , '' )
-    if( len(sentence) > 0 ):
-        return str( RNNStrToEmotion( sentence ) )
-    return 'ok'
+
+    try:
+
+        RNNStrToEmotion = loadTheRNNModel()
+        sentence = ( request.json ).get( 'sentence' , '' )
+        result = str( RNNStrToEmotion( sentence )[ 'Emotion' ][ 0 ] )
+        if( len( sentence ) > 0 ):
+            return jsonify({
+                'status' : 0,
+                'result' : result
+            })
+        else:
+            raise ValueError( 'empty sentence' )
+
+    except Exception as e:
+
+        logger.info( 'repr(e):\n'            + repr( e ) )
+        logger.info( 'traceback.format_exc():\n%s' % traceback.format_exc() )
+        return jsonify({
+            'status' : -1,
+            'result' : 'error'
+        })
+
 
 loadTheRNNModel()
+
